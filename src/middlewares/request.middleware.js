1eef462c81d340b26e668fb06b00112d96b7d401
ramyaranganathan-1775 <ramya.ranganathan@tarento.com>
2026-09-09 19:03:06 +0530

    KB-15457: Mdo Portal Comprehensive assessment Publish api

--- src/middlewares/request.middleware.js
+++ src/middlewares/request.middleware.js
@@ -568,10 +568,10 @@ function checkChannelID (req, res, next) {
 }
 
 function seteTextbook (req, res, next) {
-  if(!_.isEmpty(req.body.request) && !_.isEmpty(req.body.request.content)) {
+  if (!_.isEmpty(req.body.request) && !_.isEmpty(req.body.request.content)) {
     req.body.request.content['contentType'] = 'eTextBook'
   }
-  console.log("After Set e-Textbook: " + JSON.stringify(req.body))
+  console.log('After Set e-Textbook: ' + JSON.stringify(req.body))
   next()
 }
 
